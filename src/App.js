2fea517757232af2e2a770491817e9054d5bb491
Terence Chambers <terencechambers91@gmail.com>
2020-01-01 21:31:40 -0500

    implement grid table

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import React from 'react';
 import TabRoutes from './Components/TabRoutes';
 import { Link } from 'react-router-dom';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+import Table from './Components/Table';
 
 function App() {
   return (
     <div style={{ height: '175px', position: 'relative' }}>
       <div className='demo-big-content'>
         <Layout>
-          <Header title='Title' scroll>
+          <Header className='header-color' title='SnkrCode' scroll>
             <Navigation>
               <Link to='/task'>Task</Link>
-              <Link to='/proxies'>Prox</Link>
+              <Link to='/proxies'>Proxies</Link>
               <Link to='/billing'>Billing</Link>
               <Link to='/profile'>Profile</Link>
               <Link to='/settings'>Settings</Link>
             </Navigation>
           </Header>
-          <Drawer title='Title'>
+          <Drawer title='SnkrCode'>
             <Navigation>
               <Link to='/task'>Task</Link>
-              <Link to='/proxies'>Prox</Link>
+              <Link to='/proxies'>Proxies</Link>
               <Link to='/billing'>Billing</Link>
               <Link to='/profile'>Profile</Link>
               <Link to='/settings'>Settings</Link>
@@ -30,6 +31,7 @@ function App() {
           <Content>
             <div className='page-content' />
             <TabRoutes />
+            <Table />
           </Content>
         </Layout>
       </div>
