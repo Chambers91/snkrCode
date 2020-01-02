eaf0b5965300b1ac5c396d2cb161d8da5186bbd7
Terence Chambers <terencechambers91@gmail.com>
2020-01-01 20:07:02 -0500

    reconfigured routes and navigation bar

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import './App.css';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import TabRoutes from './Components/TabRoutes';
+import { Link } from 'react-router-dom';
 import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 function App() {
   return (
-    <div style={{ height: '300px', position: 'relative' }}>
-      <Layout
-        style={{
-          background:
-            'url(http://www.getmdl.io/assets/demos/transparent.jpg) center / cover'
-        }}
-      >
-        <Header transparent title='SnkrCode' style={{ color: 'white' }}>
-          <Navigation>
-            <Link to='/task'>Task</Link>
-            <Link to='/proxies'>Proxies</Link>
-            <Link to='/billing'>Billing</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/settings'>Settings</Link>
-          </Navigation>
-        </Header>
-        <Drawer title='Settings'>
-          <Navigation>
-            <Link to='/task'>Task</Link>
-            <Link to='/proxies'>Proxies</Link>
-            <Link to='/billing'>Billing</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/settings'>Settings</Link>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <div className='page-content' />
-          <TabRoutes />
-        </Content>
-      </Layout>
+    <div style={{ height: '175px', position: 'relative' }}>
+      <div className='demo-big-content'>
+        <Layout>
+          <Header title='Title' scroll>
+            <Navigation>
+              <Link to='/task'>Task</Link>
+              <Link to='/proxies'>Prox</Link>
+              <Link to='/billing'>Billing</Link>
+              <Link to='/profile'>Profile</Link>
+              <Link to='/settings'>Settings</Link>
+            </Navigation>
+          </Header>
+          <Drawer title='Title'>
+            <Navigation>
+              <Link to='/task'>Task</Link>
+              <Link to='/proxies'>Prox</Link>
+              <Link to='/billing'>Billing</Link>
+              <Link to='/profile'>Profile</Link>
+              <Link to='/settings'>Settings</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className='page-content' />
+            <TabRoutes />
+          </Content>
+        </Layout>
+      </div>
     </div>
   );
 }
