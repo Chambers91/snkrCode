a049b364349b9db443b6429520ad017a2000535c
Terence Chambers <terencechambers91@gmail.com>
2020-01-02 19:52:52 -0500

    implemented billing form

--- src/Pages/Billing.js
+++ src/Pages/Billing.js
@@ -4,7 +4,35 @@ class BillingPage extends Component {
   render() {
     return (
       <div>
-        <h1>Billing</h1>
+        <form className='form-centered'>
+          <br />
+          <label>
+            Name:
+            <input type='text' name='name' />
+          </label>
+          <br />
+          <label>
+            Phone:
+            <input type='text' name='phone' />
+          </label>
+          <br />
+          <label>
+            Email:
+            <input type='text' name='email' />
+          </label>
+          <br />
+          <label>
+            Billing Address:
+            <input type='text' name='billing' />
+          </label>
+          <br />
+          <label>
+            Card Number:
+            <input type='number' name='card' />
+          </label>
+          <br />
+          <input type='submit' value='Submit' />
+        </form>
       </div>
     );
   }
